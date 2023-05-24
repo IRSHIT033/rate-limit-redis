b0a0570753f311762ab9a81bd34fd17d5c1bd20d
irshit033 <mukherjeeirshit50@gmail.com>
2023-05-25 03:04:05 +0530

    fixed everything

--- main.go
+++ main.go
@@ -3,123 +3,94 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 )
 
-	type RedisClient struct {
-		client *redis.Client
+type RedisClient struct {
+	client *redis.Client
+}
+
+var (
+	redisClient *RedisClient
+)
+
+const (
+	redisAddress = "localhost:6379"
+)
+
+func GetRedisClient() *RedisClient {
+
+	redisClient = &RedisClient{
+		client: redis.NewClient(&redis.Options{
+			Addr: redisAddress,
+			DB:   0,
+		}),
 	}
 
-	var (
-		redisClient *RedisClient
-		once        sync.Once
-	)
+	return redisClient
+}
 
-	const (
-		redisAddress = "localhost:6379"
-	)
+func (rc *RedisClient) RateLimitMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-	const (
-		bucketSize  int64= 10 // Number of tokens in the bucket
-		fillRate   = 1  // Tokens filled per second
-	)
-
-	func GetRedisClient() *RedisClient {
-		
-			redisClient = &RedisClient{
-				client: redis.NewClient(&redis.Options{
-					Addr: redisAddress,
-					DB: 0,
-				}),
-			}
-
-		return redisClient
-	}
-
-	func (rc *RedisClient) RateLimitMiddleware() gin.HandlerFunc {
-		return func(c *gin.Context) {
-			// Get the current timestamp in seconds
-			currentTime := time.Now().Unix()/60
-
-			// Get the IP address
-			IPAddress := c.GetHeader("X-Real-Ip")
-			if IPAddress == "" {
-				IPAddress = c.GetHeader("X-Forwarded-For")
-			}
-			if IPAddress == "" {
-				IPAddress = c.Request.RemoteAddr
-			}
-			fmt.Println(IPAddress)
-
-			// Check if the key exists in Redis
-			exists, err := rc.client.Exists(c, IPAddress).Result()
-			if err != nil {
-				c.AbortWithStatus(http.StatusInternalServerError)
-				return
-			}
-			
-			// If the key doesn't exist, set the initial count value
-			if exists == 0 {
-				err = rc.client.Set(c, IPAddress,"0", 0).Err()
-				if err != nil {
-					c.AbortWithStatus(http.StatusInternalServerError)
-					return
-				}
-			}
-
-			// Get the count value from Redis
-			count, err := rc.client.Get(c, IPAddress).Int64()
-			fmt.Println(count)
-			if err != nil && err != redis.Nil {
-				c.AbortWithStatus(http.StatusInternalServerError)
-				return
-			}
-
-
-			// Calculate the tokens to be added to the bucket
-			tokensToAdd := (currentTime - count) * fillRate
-			fmt.Println(tokensToAdd)
-			if tokensToAdd > 0 {
-				// Add tokens to the bucket
-				pipe := rc.client.TxPipeline()
-				pipe.IncrBy(c, IPAddress, tokensToAdd)
-				pipe.Exec(c)
-			}
-			
-			// Check if the number of tokens in the bucket is sufficient
-			var bucketSize int64=10
-			if count >= bucketSize {
-				c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
-					"error": "Rate limit exceeded",
-				})
-				return
-			}
-
-			// Consume one token from the bucket
-			rc.client.Decr(c, IPAddress)
-		
-			
-			c.Next()
+		// Get the IP address
+		IPAddress := c.GetHeader("X-Real-Ip")
+		if IPAddress == "" {
+			IPAddress = c.GetHeader("X-Forwarded-For")
 		}
-	}
-
-	func PingHandler(c *gin.Context) {
-		c.String(http.StatusOK, "Pong")
-	}
-
-	func main() {
-		redisClient := GetRedisClient()
-
-		router := gin.Default()
-		router.Use(redisClient.RateLimitMiddleware())
-
-		router.GET("/ping", PingHandler)
-
-		if err := router.Run(":8080"); err != nil {
-			fmt.Println("Failed to start server:", err)
+		if IPAddress == "" {
+			IPAddress = c.Request.RemoteAddr
 		}
+		fmt.Println(IPAddress)
+
+		//getting the content of the lua script
+		script, err := os.ReadFile("script.lua")
+
+		if err != nil {
+
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"status": false, "message": "unable to read script"})
+		}
+
+		var takeScript = redis.NewScript(string(script))
+		const rate = 0.1    // 10 per second
+		const capacity = 10 // burst of up to 10
+
+		now := time.Now().UnixMicro()
+		res, err := takeScript.Run(c, rc.client, []string{IPAddress}, capacity, rate, now, 1).Result()
+		if err != nil {
+			panic(err)
+		}
+
+		allowed := res.(int64)
+		fmt.Println(allowed)
+		if allowed != 1 {
+			// request will be aborted
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{"status": false, "message": "request overflowed"})
+		}
+
+		c.Next()
 	}
+}
+
+func PingHandler(c *gin.Context) {
+	c.String(http.StatusOK, "Pong")
+}
+
+func main() {
+	redisClient := GetRedisClient()
+
+	router := gin.Default()
+	router.Use(redisClient.RateLimitMiddleware())
+
+	router.GET("/ping", PingHandler)
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
+}
+
+//seq 1 200 | xargs -n1 -P10  curl "http://localhost:8080/ping"
